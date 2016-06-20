4d8148da352220eafeffad1bf65a32ae6b13c75b
Raymon de Looff <raydelooff@gmail.com>
2016-06-20 22:55:21 +0200

    Move customizer.js, use minified scripts

--- functions.php
+++ functions.php
@@ -104,9 +104,9 @@ add_action( 'widgets_init', 'helloworld_widgets_init' );
 function helloworld_scripts() {
 	wp_enqueue_style( 'helloworld-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'helloworld-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'helloworld-vendor', get_template_directory_uri() . '/js/vendor.min.js', array(), '20160620', true );
 
-	wp_enqueue_script( 'helloworld-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'helloworld-scripts', get_template_directory_uri() . '/js/scripts.min.js', array(), '20160620', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
