962244e946a51debf3682c0caba0526a30703ef9
Raymon de Looff <raydelooff@gmail.com>
2016-06-25 00:47:10 +0200

    Change layout

--- page.php
+++ page.php
@@ -14,7 +14,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="col-xs-12 col-sm-12 col-md-9 col-lg-8 content-area">
+	<div id="primary" class="col-xs-12 col-sm-12 col-md-8 col-lg-8 content-area">
 		<main id="main" class="site-main" role="main">
 
 			<?php
--- sidebar.php
+++ sidebar.php
@@ -12,6 +12,6 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 }
 ?>
 
-<aside id="secondary" class="col-xs-12 col-sm-12 col-md-3 col-lg-4 widget-area" role="complementary">
+<aside id="secondary" class="col-xs-12 col-sm-12 col-md-4 col-lg-4 widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
